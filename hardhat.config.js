dfd9acdc901c3c203defdfcde578cb63e1fc85dc
Matias Falcone <matias.falcone@gmail.com>
2024-04-19 16:02:51 +1000

    added rpc, explorer and api smart contract verification urls

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const MNEMONIC_DEVNET = vars.get("MNEMONIC_DEVNET");
+const MNEMONIC_MAINNET = vars.get("MNEMONIC_MAINNET");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -10,5 +13,51 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  networks: {
+    localhost: {
+      gas: 19000000,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+      accounts: { mnemonic: `${MNEMONIC_DEVNET}` },
+    },
+    hardhat: {
+      gas: 19000000,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+      accounts: { mnemonic: `${MNEMONIC_DEVNET}` },
+    },
+    devnet: {
+      url: "https://rpc-devnet-cardano-evm.c1.milkomeda.com",
+      chainId: 200101,
+      gasPrice: 30000000000,
+      accounts: { mnemonic: `${MNEMONIC_DEVNET}` },
+    },
+    mainnet: {
+      url: "https://rpc-mainnet-cardano-evm.c1.milkomeda.com",
+      chainId: 2001,
+      gasPrice: 30000000000,
+      accounts: { mnemonic: `${MNEMONIC_MAINNET}` },
+    },
+  },
+  etherscan: {
+    customChains: [
+      {
+        network: "milkomeda-c1-devnet",
+        chainId: 200101,
+        urls: {
+          apiURL: "https://explorer-devnet-cardano-evm.c1.milkomeda.com/api",
+          browserURL: "https://explorer-devnet-cardano-evm.c1.milkomeda.com"
+        }
+      },
+      {
+        network: "milkomeda-c1-mainnet",
+        chainId: 2001,
+        urls: {
+          apiURL: "https://explorer-mainnet-cardano-evm.c1.milkomeda.com/api",
+          browserURL: "https://explorer-mainnet-cardano-evm.c1.milkomeda.com"
+        }
+      }
+    ]
   },
 };
